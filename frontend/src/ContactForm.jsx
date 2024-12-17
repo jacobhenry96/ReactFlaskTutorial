c939ae1691126df34673a9e88bd60dd2adb06b61
jacobhenry96 <jacobkhenry2@gmail.com>
2024-12-17 06:18:47 -0600

    complete contact form

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -5,13 +5,28 @@ const ContactForm = ({}) => {
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
 
-    const onSubmit = () => {
+    const onSubmit = async (e) => {
         e.preventDefault()
 
         const data = {
             firstName,
             lastName,
             email
+        }
+        const url = 'http://127.0.0.1:5000/create_contact'
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status ==! 201 && response.status !== 200) {
+            const message = await response.json()
+            alert(data.message)
+        } else {
+
         }
     }
 
